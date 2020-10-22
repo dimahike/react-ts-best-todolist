daf6d8df821444119f3534cc8760b222de36acdf
dimahike <dimahike@gmail.com>
2020-10-22 19:07:37 +0300

    Updates

---
+++ precache-manifest.07e1a90035c64ff4e70af3afc96ea57e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4885ec3cafdcc7f14d39d772d8173e07",
+    "url": "/react-ts-best-todolist/index.html"
+  },
+  {
+    "revision": "493536755e70a0b9e203",
+    "url": "/react-ts-best-todolist/static/css/main.6cd06783.chunk.css"
+  },
+  {
+    "revision": "825add9faecccfbe3404",
+    "url": "/react-ts-best-todolist/static/js/2.02552950.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-ts-best-todolist/static/js/2.02552950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "493536755e70a0b9e203",
+    "url": "/react-ts-best-todolist/static/js/main.ddc0f022.chunk.js"
+  },
+  {
+    "revision": "18b21602aeca346a58b5",
+    "url": "/react-ts-best-todolist/static/js/runtime-main.2b798c9f.js"
+  }
+]);
